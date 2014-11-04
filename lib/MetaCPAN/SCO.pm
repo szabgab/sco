002c9f2ae80b41cc9e747d0aebb4eb105ad929ed
Gabor Szabo <gabor@szabgab.com>
2014-11-04 10:59:09 +0200

    Start using Template toolkit to show the empty pages

--- lib/MetaCPAN/SCO.pm
+++ lib/MetaCPAN/SCO.pm
@@ -5,6 +5,9 @@ use warnings;
 our $VERSION = '0.01';
 
 use Plack::Request;
+use Template;
+use Cwd qw(abs_path);
+use File::Basename qw(dirname);
 
 =head1 NAME
 
@@ -18,11 +21,34 @@ sub run {
 
         my $request = Plack::Request->new($env);
         if ($request->path_info eq '/') {
-            return [ 200, [ 'Content-Type' => 'text/plain' ], ['Hello'] ];
+            return template('index');
         }
         return [ 404, [ 'Content-Type' => 'text/html' ], ['404 Not Found'] ];
 
     };
+}
+
+sub template {
+    my ($file) = @_;
+
+
+    my $root = dirname(dirname(dirname(abs_path(__FILE__))));
+
+    my $tt = Template->new(
+        INCLUDE_PATH => "$root/tt",
+        INTERPOLATE  => 0,
+        POST_CHOMP   => 1,
+        EVAL_PERL    => 0,
+        START_TAG    => '<%',
+        END_TAG      => '%>',
+        PRE_PROCESS  => 'incl/header.tt',
+        POST_PROCESS => 'incl/footer.tt',
+    );
+
+    my $out;
+    $tt->process( "$file.tt", {}, \$out) || die $tt->error();
+
+    return [ 200, [ 'Content-Type' => 'text/html' ], [$out] ];
 }
 
 1;
